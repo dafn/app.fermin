49eb925d630cd2bbd8a5fefd97204f0a02d5dda8
dafn <dafn@Netlights-MacBook-Pro.local>
2019-01-17 23:16:31 +0100

    add some security and defer sw

--- app/server/app.js
+++ app/server/app.js
@@ -1,18 +1,25 @@
-const 
+const
   express = require('express'),
   compression = require('compression'),
   bodyParser = require('body-parser'),
   path = require('path'),
   main = require('./routes/main'),
   notes = require('./routes/notes'),
-  app = express()
-
-const port = process.env.PORT || 8002
+  app = express(),
+  port = process.env.PORT || 8002
 
 app.use(compression())
+app.use(express.static(path.resolve(__dirname, '../client/dist/')))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(express.static(path.resolve(__dirname, '../client/dist/')))
+
+app.use((req, res, next) => {
+  res.setHeader('Content-Security-Policy', 'default-src "self"')
+  res.setHeader('X-XSS-Protection', '1; mode=block')
+  res.setHeader('X-Frame-Options', 'DENY')
+  next()
+})
 
 app.use('/', main)
 app.use('/notes', notes)
