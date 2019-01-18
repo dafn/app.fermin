81fe4dcc3bff9104f35e2da12ba0a9e1eedc9b1c
dafn <dafn@Netlights-MacBook-Pro.local>
2019-01-18 13:53:54 +0100

    Add more error handling, https redirection and security headers

--- app/server/app.js
+++ app/server/app.js
@@ -8,18 +8,23 @@ const
   app = express(),
   port = process.env.PORT || 8002
 
+app.use((req, res, next) =>
+  req.protocol != 'https' && process.env.NODE_ENV !== 'development' 
+    ? res.redirect('https://' + req.hostname + ':' + port + req.baseUrl)
+    : next()
+)
+app.enable('trust proxy')
+
 app.use(compression())
-app.use(express.static(path.resolve(__dirname, '../client/dist/')))
+app.use(express.static(path.resolve(__dirname, '../client/dist/'), {
+  setHeaders: res => {
+    res.set('X-XSS-Protection', '1; mode=block')
+    res.set('X-Frame-Options', 'DENY')
+  }
+}))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-
-app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', 'default-src "self"')
-  res.setHeader('X-XSS-Protection', '1; mode=block')
-  res.setHeader('X-Frame-Options', 'DENY')
-  next()
-})
 
 app.use('/', main)
 app.use('/notes', notes)
--- app/server/routes/notes.js
+++ app/server/routes/notes.js
@@ -2,27 +2,34 @@ const router = require('express').Router(),
   { addNote, updateNote, deleteNote, listNotes } = require('../datastore')
 
 router.post('/add', async (req, res) => {
-
-  let transaction = await addNote(req.body.user, req.body.content)
-
-  if (transaction === 500)
-    res.sendStatus(500)
+  if (req.body.user && req.body.content)
+    if (await addNote(req.body.user, req.body.content) === 500)
+      res.sendStatus(500)
+    else
+      res.send({ id: transaction })
   else
-    res.send({
-      id: transaction
-    })
+    res.sendStatus(400)
 })
 
 router.post('/update', async (req, res) => {
-  res.sendStatus(await updateNote(req.body.id, req.body.content))
+  if (req.body.id && req.body.content)
+    res.sendStatus(await updateNote(req.body.id, req.body.content))
+  else
+    res.sendStatus(400)
 })
 
 router.get('/delete/:id', async (req, res) => {
-  res.sendStatus(await deleteNote(req.params.id))
+  if (req.params.id)
+    res.sendStatus(await deleteNote(req.params.id))
+  else
+    res.sendStatus(400)
 })
 
 router.get('/list/:user', async (req, res) => {
-  res.send(await listNotes(req.params.user))
+  if (req.params.user)
+    res.send(await listNotes(req.params.user))
+  else
+    res.sendStatus(400)
 })
 
 module.exports = router
