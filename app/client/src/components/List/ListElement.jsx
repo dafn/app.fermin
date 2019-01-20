dcff3f609a1061a79d8782c8ee3e6f3795175c90
dafn <dafn@Netlights-MacBook-Pro.local>
2019-01-20 10:46:02 +0100

    Remove duplicate ids

--- app/client/src/components/List/ListElement.jsx
+++ app/client/src/components/List/ListElement.jsx
@@ -14,20 +14,20 @@ const ListElement = ({ index, value, active }) => {
       {
         ({ actions: { setActiveKey, showModal } }) => {
           return (
-            <div id='ListElement_main_container' className={active ? 'active' : ''} onClick={() => setActiveKey(index)}>
-              <div id='ListElement_text_container'>
-                <div id='ListElement_title_container'>
+            <div className={active ? 'ListElement_main_container active' : 'ListElement_main_container'} onClick={() => setActiveKey(index)}>
+              <div className='ListElement_text_container'>
+                <div className='ListElement_title_container'>
                 {
                   value && value.match(/[^<p>].*?(?=<)/g)[0] !== 'br>'
-                  ? <ReactQuill id='List_element_editor' value={value.match(/[^<p>].*?(?=<)/g)[0]} readOnly modules={modules} />
+                  ? <ReactQuill className='ListElement_editor' value={value.match(/[^<p>].*?(?=<)/g)[0]} readOnly modules={modules} />
                   : 'Untitled Note'
                 }
                 </div>
-                <div id='ListElement_summary' >
-                  <ReactQuill id='List_element_editor' value={value ? value.replace(value.match(/[^<p>].*?(?=<)/g)[0], '') : ''} readOnly modules={modules} />
+                <div className='ListElement_summary' >
+                  <ReactQuill className='ListElement_editor' value={value ? value.replace(value.match(/[^<p>].*?(?=<)/g)[0], '') : ''} readOnly modules={modules} />
                 </div>
               </div>
-              <div id='ListElement_delete_button' >
+              <div className='ListElement_delete_button' >
                 <p onClick={event => { event.stopPropagation(); showModal() }}>x</p>
               </div>
             </div>
