26b128f5188ec7b1402b53789948cc349dfb1a87
dafn <dafn@Netlights-MacBook-Pro.local>
2019-02-03 00:16:07 +0100

    Replace datastore with redis for session storage

--- app/server/app.js
+++ app/server/app.js
@@ -1,10 +1,9 @@
 const
   { authenticate, isAuthenticated } = require('./authentication/openid'),
   { session_store_key } = require('../../keys/session-store'),
-  session = require('express-session'),
   express = require('express'),
-  datastore = require('@google-cloud/datastore'),
-  sessionstore = require('@google-cloud/connect-datastore')(session),
+  session = require('express-session'),
+  redis = require('connect-redis')(session),
   path = require('path'),
   notes = require('./routes/notes'),
   passport = require('passport'),
@@ -23,13 +22,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(require('compression')())
 app.use(require('body-parser').json())
 app.use(session({
-  store: new sessionstore({
-    dataset: new datastore({
-      kind: 'express-sessions',
-      projectId: 'no-fermin',
-      keyFilename: 'keys/datastore-service-account-key.json'
-    })
-  }),
+  store: new redis({ port: 6379 }),
   secret: session_store_key,
   resave: false,
   saveUninitialized: false,
@@ -52,3 +45,4 @@ app.use(isAuthenticated, express.static(path.resolve(__dirname, '../client/dist/
 }))
 
 app.listen(port, () => console.log(`listening on port ${port}`))
+
