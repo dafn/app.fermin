cbaad1a698e8995bffb807275ae447b0a42fbcf9
dafn <dafn@Netlights-MacBook-Pro.local>
2019-02-03 14:30:47 +0100

    add thumbs up to console log

--- app/server/app.js
+++ app/server/app.js
@@ -1,6 +1,7 @@
 const
   { authenticate, isAuthenticated } = require('./authentication/openid'),
   { session_store_key } = require('../../keys/session-store'),
+  { Terminal } = require('./terminal/colors'),
   express = require('express'),
   session = require('express-session'),
   redis = require('connect-redis')(session),
@@ -21,6 +22,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(require('compression')())
 app.use(require('body-parser').json())
+
 app.use(session({
   store: new redis({ port: 6379 }),
   secret: session_store_key,
@@ -30,7 +32,6 @@ app.use(session({
   cookie: { secure: process.env.NODE_ENV === 'production' },
   maxAge: 2628000
 }))
-
 app.use(passport.initialize())
 app.use(passport.session())
 
@@ -44,5 +45,4 @@ app.use(isAuthenticated, express.static(path.resolve(__dirname, '../client/dist/
   }
 }))
 
-app.listen(port, () => console.log(`listening on port ${port}`))
-
+app.listen(port, () => console.log(`listening on port ${port} 👍`))
