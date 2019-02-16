4887f53af67b47a5f1a35e2ac14c3e2c61d90376
dafn <dafn@Netlights-MacBook-Pro.local>
2019-02-16 08:26:35 +0100

    Add new add-card button

--- app/client/src/apps/hub/index.js
+++ app/client/src/apps/hub/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Card from './src/components/Card'
+import Add from './src/components/Add'
 import Section from './src/components/Section'
 
 import './src/sass/main.sass'
@@ -10,6 +11,7 @@ const App = props => {
     <div id='fermin_hub'>
       <Section title='Internal Applications' >
         <Card title='Notes' internal href='app/notes' background='#333333' color='white' image={require(`./assets/pen.png`)} />
+        <Add />
       </Section>
       <Section title='External Applications'>
         <Card title='Dashlane' href='https://www.dashlane.com/' background='linear-gradient(180deg,#005a7d,#007c97)' color='white' image={require('./assets/dashlane.png')} />
@@ -18,6 +20,7 @@ const App = props => {
         <Card title='Github' href='https://github.com/' background='#24292e' color='white' image={require('./assets/github.png')} />
         <Card title='One' href='https://www.one.com/no/' background='#74a345' color='white' image={require('./assets/one.com.png')} />
         <Card title='Microsoft' href='https://products.office.com/nb-no/compare-all-microsoft-office-products?tab=2&OCID=AID679471_OO_Hero_mscomrefreshhome' image={require('./assets/microsoft.png')} />
+        <Add />
       </Section>
     </div>
   )
---
+++ app/client/src/apps/hub/src/components/Add/Add.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <article id='add_container'>
+      <h1 id='add_text'>+</h1>
+    </article>
+  )
+}
+
+export default Add
