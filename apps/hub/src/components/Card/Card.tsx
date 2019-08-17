30060de27eca9a4df9de7e4a2f0160d5398a9d2e
dafn <dafn@dafns-MacBook-Pro.local>
2019-08-17 21:35:47 +0200

    fix straange rendering problem

--- apps/hub/src/components/Card/Card.tsx
+++ apps/hub/src/components/Card/Card.tsx
@@ -8,18 +8,20 @@ import './Card.sass'
 const Card = (props: CardProps) => {
 
   const { card: { image, title, background= 'white', textColor = '#333333', internal, href } } = props
-  const [state, setState] = useState({ mounted: '', imageHidden: false })
+
+  const [mounted, setMounted] = useState('')
+  const [imageHidden, setImageHidden] = useState(false)
 
   useEffect(() => {
-    setState({ ...state, mounted: 'mounted' })
-  }, [state.mounted])
+    setMounted('mounted')
+  }, [mounted])
 
   return (
     <section>
       {
         internal
           ?
-          <Link to={href || '/'} className={`card ${state.mounted || ''}`} name={title} style={{ background: `${background}` }}>
+          <Link to={href || '/'} className={`card ${mounted || ''}`} name={title} style={{ background: `${background}` }}>
             <div className='card_image' >
               <img src={image} alt='card image' />
             </div>
@@ -28,9 +30,9 @@ const Card = (props: CardProps) => {
             </div>
           </Link>
           :
-          <a className={`card ${state.mounted || ''}`} href={href || '#'} target={href && '_blank'} style={{ background: `${background}` }} rel='noopener'>
+          <a className={`card ${mounted || ''}`} href={href || '#'} target={href && '_blank'} style={{ background: `${background}` }} rel='noopener'>
             <div className='card_image'>
-              <img src={image} alt='card image' className={state.imageHidden ? 'hidden': 'visible'} onLoad={() => setState({...state, imageHidden: false })} onError={() => setState({ ...state, imageHidden: true })}/>
+              <img src={image} alt='card image' className={imageHidden ? 'hidden': 'visible'} onLoad={() => setImageHidden(false)} onError={() => setImageHidden(true)}/>
             </div>
             <div className='card_text'>
               <p style={{ color: `${textColor}` }}>{title}</p>
