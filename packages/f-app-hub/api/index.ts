4fc4d3bb66299bf6ba30ca84f31014688828f104
Daniel Nilsen <dafn@outlook.com>
2019-09-04 12:49:29 +0200

    tiny refactor of hub api

--- packages/f-app-hub/api/index.ts
+++ packages/f-app-hub/api/index.ts
@@ -4,8 +4,8 @@ import { CardType, onErrorType, onSuccessType } from './types'
 const graphql = new GraphQLClient("/gql", { credentials: "same-origin" })
 
 export const datastore = {
-  addCard: ({ title, href, background, image, textColor }: CardType, onSuccess?: onSuccessType, onError?: onErrorType ) => {
-    graphql.request(`mutation { addCard(title: "${title}", href: "${href || '#'}", background: "${background || 'white'}", textColor: "${textColor || 'black'}", image: "${image}") }`)
+  addCard: ({ title, href = '#', background = 'white', textColor = 'black', image }: CardType, onSuccess?: onSuccessType, onError?: onErrorType ) => {
+    graphql.request(`mutation { addCard(title: "${title}", href: "${href}", background: "${background}", textColor: "${textColor}", image: "${image}") }`)
       .then(response => { onSuccess && onSuccess(response) })
       .catch(err => {
         if (err.response.status === 401) window.location.href = "/auth/login"
