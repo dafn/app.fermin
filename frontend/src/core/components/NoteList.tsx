2921972893f1ad480168e7f1c19f71d893ea4312
dafn <dafn@dafns-MacBook-Pro.local>
2020-04-21 23:21:57 +0200

    add delete prompt

--- frontend/src/core/components/NoteList.tsx
+++ frontend/src/core/components/NoteList.tsx
@@ -1,10 +1,11 @@
 import { h } from "preact";
 
-import { useContext } from "preact/hooks";
+import { useContext, useRef } from "preact/hooks";
 
 import Icon from "preact-material-components/Icon";
 import Card from "preact-material-components/Card";
 import Fab from "preact-material-components/Fab";
+import Dialog from "preact-material-components/Dialog";
 
 import context from "src/pages/notebook/context";
 
@@ -18,6 +19,8 @@ const Notelist = () => {
     setActiveIndex,
     deleteNote,
   } = useContext(context);
+
+  let deletePromt = useRef(null);
 
   const addNote = () => {
     notes.push({
@@ -40,7 +43,7 @@ const Notelist = () => {
             <p class="mdc-typography--body2"> {note.content} </p>
             <Icon
               class={`${index === activeIndex ? style.show : ""}`}
-              onClick={() => deleteNote(activeIndex)}
+              onClick={() => deletePromt["MDComponent"].show()}
             >
               delete_outline
             </Icon>
@@ -54,6 +57,16 @@ const Notelist = () => {
       <Fab ripple mini onClick={addNote}>
         <Fab.Icon>add</Fab.Icon>
       </Fab>
+      <Dialog
+        ref={(dialog) => (deletePromt = dialog)}
+        onAccept={() => deleteNote(activeIndex)}
+      >
+        <Dialog.Header>Er du sikker på at du vil slette notatet?</Dialog.Header>
+        <Dialog.Footer>
+          <Dialog.FooterButton cancel>Nei</Dialog.FooterButton>
+          <Dialog.FooterButton accept>Ja</Dialog.FooterButton>
+        </Dialog.Footer>
+      </Dialog>
     </section>
   );
 };
