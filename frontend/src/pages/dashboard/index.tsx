5e1ea2e85db6b163923369be01eaa504646d16a8
dafn <dafn@dafns-MacBook-Pro.local>
2020-05-05 20:50:14 +0200

    add e24 and fn as iframes

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -2,18 +2,11 @@ import { h } from "preact";
 
 import style from "./index.module.scss";
 
-import BookmarksCard from "src/components/BookmarksCard";
-import Bookmark from "src/components/Bookmark";
 const Dashboard = () => {
   return (
     <main class={style.dashboard}>
-      <BookmarksCard>
-        <Bookmark
-          img="https://www.fermin.no/favicon.71affc08.png"
-          href="https://www.fermin.no/"
-        />
-      </BookmarksCard>
       <iframe src="https://bors.e24.no/#!/market/world"></iframe>
+      <iframe src="https://finansavisen.no"></iframe>
     </main>
   );
 };
