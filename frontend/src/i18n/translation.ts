7717f8aca5870149c921585699bc682b8f0e7ec4
dafn <dafn@dafns-MacBook-Pro.local>
2020-06-11 01:35:48 +0200

    add more translations

--- frontend/src/i18n/translation.ts
+++ frontend/src/i18n/translation.ts
@@ -3,6 +3,34 @@ const translations = {
     no: "Akkumulert formue",
     en: "Accumulated Wealth",
   },
+  private: {
+    no: "Privat",
+    en: "Private",
+  },
+  corporate: {
+    no: "AS",
+    en: "Corporate",
+  },
+  independent_earnings: {
+    no: "Selvstendig overskudd",
+    en: "Independent earnings",
+  },
+  gross_income: {
+    no: "Brutto inntekt",
+    en: "Gross income",
+  },
+  salary: {
+    no: "Lønn",
+    en: "Salary",
+  },
+  tax_on_salary: {
+    no: "Skatt på lønn i %",
+    en: "Tax on salary in %",
+  },
+  total: {
+    no: "Total",
+    en: "Total",
+  },
 };
 
 export default (key: string, language: Language) => {
