04d8b4505f29d7b0707b128ca87f5172e1e9daf9
dafn <dafn@dafns-MacBook-Pro.local>
2020-06-14 16:38:52 +0200

    move stil cache to memory and clean it

--- frontend/plugins/rollup-plugin-stil.js
+++ frontend/plugins/rollup-plugin-stil.js
@@ -1,15 +1,8 @@
-import fs from "fs";
 import path from "path";
-import rimraf from "rimraf";
 import * as babel from "@babel/core";
 import murmurhash3 from "murmurhash3";
 
-const tempDir = ".stil_temp";
-const dirname = path.resolve();
-
-let outputFileName;
-let outputFilePath;
-// let counter = 0;
+let extractedStyles = {};
 
 const presets = {
   ".ts": ["@babel/preset-typescript"],
@@ -23,7 +16,7 @@ const plugins = {
 
 const supportedFormats = [".js", ".ts", ".jsx", ".tsx"];
 
-const transform = (filepath, identifier, raw) => {
+const transform = (filepath, raw) => {
   const ast = babel.transformSync(raw, {
     babelrc: false,
     configFile: false,
@@ -37,7 +30,6 @@ const transform = (filepath, identifier, raw) => {
   let style, styleIndex;
 
   for (let [index, node] of Object.entries(ast.program.body)) {
-    // css``
     if (
       node.type === "ExpressionStatement" &&
       node.expression.type === "TaggedTemplateExpression" &&
@@ -51,43 +43,11 @@ const transform = (filepath, identifier, raw) => {
       styleIndex = index;
       style = node.expression.quasi.quasis[0].value.raw;
     }
-
-    // const style = ``;
-    /* if (node.type === "VariableDeclaration" && node.kind === "const") {
-      for (let declaration of node.declarations) {
-        if (declaration.id["name"] === identifier) {
-          if (declaration.init.type !== "TemplateLiteral")
-            throw new Error(
-              `stil value must be a TemplateLiteral > ${filepath}:${declaration.loc.start.line}:1`
-            );
-          if (declaration.init.quasis.length > 1)
-            throw new Error(
-              `stil does not support string interpolation > ${filepath}:${declaration.loc.start.line}:1`
-            );
-
-          styleIndex = index;
-          style = declaration.init.quasis[0].value.raw;
-        }
-      }
-    } */
   }
 
   if (typeof style !== "string") return undefined;
 
   ast.program.body.splice(styleIndex, 1);
-
-  const importcss = babel.transformSync(
-    `import ${identifier} from "${dirname}/${outputFilePath}"`,
-    {
-      filename: filepath,
-      babelrc: false,
-      configFile: false,
-      ast: true,
-      code: false,
-    }
-  );
-
-  ast.program.body.unshift(importcss.ast.program.body[0]);
 
   let code = babel.transformFromAstSync(ast, null, {
     filename: filepath,
@@ -101,43 +61,36 @@ const transform = (filepath, identifier, raw) => {
   };
 };
 
-const defaultOptions = {
-  identifier: "css",
-  extension: "css",
-};
+export default (options = defaultOptions) => {
+  options = {
+    extension: "css",
+    ...options,
+  };
 
-export default function myPlugin(options = defaultOptions) {
   return {
     name: "stil",
+    load(id) {
+      return extractedStyles[id];
+    },
+    resolveId(importee) {
+      return extractedStyles[importee] && importee;
+    },
     transform(code, id) {
       if (!supportedFormats.includes(path.extname(id))) return;
-      if (!fs.existsSync(tempDir)) fs.mkdirSync(tempDir);
 
-      // if (!supportedFormats.includes(path.basename(id))) return;
+      const outputFileName = murmurhash3.murmur32HexSync(id);
+      const outputFilePath = `/${outputFileName}.module.${options.extension}`;
 
-      outputFileName = murmurhash3.murmur32HexSync(id);
-      outputFilePath = `${tempDir}/${outputFileName}.module.${
-        options.extension || defaultOptions.extension
-      }`;
+      const transformedCode = transform(id, code);
 
-      const transformedCode = transform(
-        id,
-        options.identifier || defaultOptions.identifier,
-        code
-      );
+      if (!transformedCode) return;
 
-      if (transformedCode) {
-        // TODO: use this.emitFile
-        fs.writeFileSync(outputFilePath, transformedCode.style);
-      }
+      extractedStyles[outputFilePath] = transformedCode.style;
 
       return {
-        code: transformedCode ? transformedCode.code : code,
+        code: `import css from "${outputFilePath}"; ${transformedCode.code}`,
         map: null,
       };
     },
-    buildEnd() {
-      if (process.env.ROLLUP_WATCH !== "true") rimraf.sync(tempDir);
-    },
   };
-}
+};
