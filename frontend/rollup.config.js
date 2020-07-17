a70c695fa3f6decaacbdb5025f9a425de45757cc
dafn <dafn@dafns-MacBook-Pro.local>
2020-07-17 11:50:34 +0200

    add favicon

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -19,6 +19,11 @@ const template = `
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link
+        rel="shortcut icon"
+        type="image/svg+xml"
+        href="assets/favicon.svg"
+      />
+      <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
       />
