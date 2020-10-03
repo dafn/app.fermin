eb71133d57f17d537c6f4866644ab8f1df049c12
dafn <dafn@dafns-MacBook-Pro.local>
2020-10-03 20:10:51 +0200

    add cvList to cv page

---
+++ frontend/src/components/CVList.tsx
@@ -0,0 +1,119 @@
+import { h } from "preact";
+
+import { useState } from "preact/hooks";
+
+import Button from "src/components/core/Button";
+import Fab from "src/components/core/Fab";
+
+import Dialog from "./core/Dialog";
+
+import ListElement from "./ListElement";
+
+const CVList = () => {
+  const [dialog, showDialog] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const cvs = [
+    {
+      title: "Telia",
+      content: "Telia er et telekomselskap som oppererer i hhele norden",
+      src: "https://fomantic-ui.com/images/avatar2/large/kristy.png",
+    },
+    {
+      title: "Buypass",
+      content:
+        "Buypass er nordens største TLS utgiver og har kontorer i 4 land",
+      src: "https://fomantic-ui.com/images/avatar2/large/molly.png",
+    },
+  ];
+
+  const addCV = () => {
+    cvs.push({
+      title: `${new Date().getTime()}`,
+      content:
+      `${new Date().getTime()}${new Date().getTime()}`,
+      src: "https://fomantic-ui.com/images/wireframe/image.png",
+    });
+  };
+
+  const deleteCV = (index) => {
+    delete cvs[index];
+  }
+
+  return (
+    <section className={css["notelist"]}>
+      {cvs.length ? (
+        cvs.map((cv, index) => (
+          <ListElement
+            className={css["list-element"]}
+            title={cv.title}
+            content={cv.content}
+            active={index === activeIndex}
+            src={cv.src}
+            toggle="Eksporter"
+            onClick={() => index !== activeIndex && setActiveIndex(index)}
+            onDelete={() => {
+              showDialog(true);
+            }}
+          />
+        ))
+      ) : (
+        <section className={css["empty-list"]}>
+          <h2 className="mdc-typography--subtitle2">Empty list</h2>
+        </section>
+      )}
+      <Fab className={css["fab"]} onClick={addCV}>
+        <i className="icon-plus" />
+      </Fab>
+      <Dialog message="Er du sikker på at vil slette notatet?" show={dialog}>
+        <Button variant="primary" contained onClick={() => showDialog(false)}>
+          Cancel
+        </Button>
+        <Button
+          variant="primary"
+          contained
+          onClick={() => {
+            deleteCV(activeIndex);
+            showDialog(false);
+          }}
+        >
+          Accept
+        </Button>
+      </Dialog>
+    </section>
+  );
+};
+
+export default CVList;
+
+css`
+  .notelist {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: var(--fermin-theme-tertiary);
+    padding-top: 1rem;
+    overflow-y: scroll;
+    min-width: 22rem;
+    .list-element {
+      margin-bottom: 1rem;
+    }
+    .fab {
+      background-color: var(--fermin-theme-positive);
+    }
+    .empty-list {
+      display: grid;
+      position: relative;
+      height: 3rem;
+      width: 18rem;
+      margin: 0 1rem 1rem 1rem;
+      grid-row: 1fr;
+      h2 {
+        align-self: center;
+        justify-self: center;
+        color: var(--fermin-theme-placeholder);
+        margin: 0;
+      }
+    }
+  }
+`;
--- frontend/src/pages/cv.tsx
+++ frontend/src/pages/cv.tsx
@@ -1,22 +1,12 @@
 import { h } from "preact";
 
-import CVSection from "src/components/CVSection";
+import CVList from "src/components/CVList";
 import ListElement from "src/components/ListElement";
 
 const CV = () => {
   return (
     <section className={css["cv"]}>
-      <ListElement
-        className={css["cv-list-element"]}
-        src="https://fomantic-ui.com/images/avatar2/large/kristy.png"
-        toggle="Eksporter"
-        onToggle={(state) => {
-          console.log(state);
-        }}
-        title="I am testing"
-        content="this is the content"
-        active
-      />
+      <CVList />
     </section>
   );
 };
@@ -25,8 +15,7 @@ export default CV;
 
 css`
   .cv {
-    .cv-list-element {
-      margin: 1rem;
-    }
+    display: grid;
+    grid-template-columns: auto 1fr;
   }
-`;
+`
