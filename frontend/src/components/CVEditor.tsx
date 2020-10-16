d68326375303417b0937aa499b1bce733250d2d7
dafn <dafn@dafns-MacBook-Pro.local>
2020-10-16 23:44:59 +0200

    add image hover

--- frontend/src/components/CVEditor.tsx
+++ frontend/src/components/CVEditor.tsx
@@ -50,11 +50,14 @@ const CVEditor = () => {
   return (
     <section className={css["notepad"]}>
       <div className={css["title-image-container"]}>
-        <div>
+        <div className={"title-image"}>
           <img
             src="https://fomantic-ui.com/images/wireframe/image.png"
             alt="image"
           />
+          <div className={css["add-image"]}>
+            <i className={`${css["add-image-icon"]} icon-plus`} />
+          </div>
         </div>
         <input
           type="text"
@@ -105,13 +108,47 @@ css`
       display: grid;
       grid-template-columns: auto 1fr;
       div {
+        display: grid;
         height: 4rem;
         width: 4rem;
         align-self: center;
         margin-right: 2rem;
+        cursor: pointer;
         img {
+          transition: opacity 0.1s;
           height: 100%;
           width: auto;
+        }
+        .add-image {
+          display: grid;
+          visibility: hidden;
+          opacity: 0;
+          transition: opacity 0.1s;
+          height: 100%;
+          width: 100%;
+          border: 1px dashed var(--fermin-light-on-dark);
+          .add-image-icon {
+            align-self: center;
+            justify-self: center;
+            color: var(--fermin-light-on-dark);
+          }
+          & > * {
+            grid-row: 1;
+            grid-column: 1;
+          }
+        }
+        &:hover {
+          img {
+            opacity: 0;
+          }
+          .add-image {
+            visibility: visible;
+            opacity: 1;
+          }
+        }
+        & > * {
+          grid-row: 1;
+          grid-column: 1;
         }
       }
     }
