80dfb29defecc3b6c7a3ea24863855b8d6ba9525
dafn <dafn@dafns-MacBook-Pro.local>
2020-12-19 11:38:32 +0100

    add accordion animation

--- frontend/src/components/ListSection.tsx
+++ frontend/src/components/ListSection.tsx
@@ -1,22 +1,45 @@
 import { h } from "preact";
-import { useState } from "preact/hooks";
+import { useEffect, useRef, useState } from "preact/hooks";
 import ListSectionHeader from "src/components/ListSectionHeader";
 import cn from "src/utils/cn";
 
 interface Props {
   header?: string;
+  className?: string;
   children?: h.JSX.Element | h.JSX.Element[];
 }
 
-const ListSection = ({ header, children }: Props) => {
+const ListSection = ({ header, className, children }: Props) => {
   const [visible, setVisible] = useState(true);
 
+  const elementContainer = useRef(null);
+
+  useEffect(() => {
+    // -.-
+    setTimeout(() => {
+      elementContainer.current.style.height = "min-content";
+      elementContainer.current.style.height =
+        elementContainer.current.scrollHeight + "px";
+    }, 100);
+  }, [children]);
+
   return (
-    <section className={css["list-section"]}>
+    <section
+      className={`${css["list-section"]} ${cn({
+        [className]: !!className,
+      })}`}
+    >
       {header && (
         <ListSectionHeader
           text={header}
           onToggle={(toggled) => {
+            if (toggled) {
+              elementContainer.current.style.height =
+                elementContainer.current.scrollHeight + "px";
+            } else {
+              elementContainer.current.style.height = 0 + "px";
+            }
+
             setVisible(toggled);
           }}
         />
@@ -25,6 +48,8 @@ const ListSection = ({ header, children }: Props) => {
         className={` ${css["list-elements"]} ${cn({
           [css["hidden"]]: !visible,
         })}`}
+        ref={elementContainer}
+        aria-hidden={!visible}
       >
         {children}
       </div>
@@ -38,14 +63,11 @@ css`
   .list-section {
     background-color: inherit;
     .list-elements {
-      opacity: 1;
+      transition: height 0.3s, visibility 0.3s;
       visibility: visible;
       overflow: hidden;
-      max-height: min-content;
       &.hidden {
-        opacity: 0;
         visibility: hidden;
-        max-height: 0;
       }
     }
   }
